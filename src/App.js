b8f443520fd3d19e88af3397a8a17b1885e3d751
rahulyadav139 <ry.rahulmyadav@gmail.com>
2022-04-23 01:08:33 +0530

    docs: homepage added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<AuthPage />} />
+        <Route path="/" element={<Homepage />} />
       </Routes>
     </div>
   );
---
+++ src/components/social-media-card/SocialMediaCard.js
@@ -0,0 +1,47 @@
+import './SocialMediaCard.css';
+
+const SocialMediaCard = props => {
+  return (
+    <div class="card shadow social">
+      <div class="flex space-between align-center">
+        <div class="flex gap align-center">
+          <div class="avatar small">
+            <img
+              src="https://i.picsum.photos/id/933/536/354.jpg?hmac=8lVRoNcysARFInMz443q-mc0wbgwHbJgFe5ChEo-YaQ"
+              alt=""
+            />
+          </div>
+          <div class="flex col">
+            <div class="user">Michael</div>
+            <div class="time">Today</div>
+          </div>
+        </div>
+        <button class="btn icon medium primary">
+          <i class="fas fa-ellipsis-v"></i>
+        </button>
+      </div>
+      <div class="image">
+        <img
+          class="img-responsive"
+          src="https://i.picsum.photos/id/933/536/354.jpg?hmac=8lVRoNcysARFInMz443q-mc0wbgwHbJgFe5ChEo-YaQ"
+          alt=""
+        />
+      </div>
+      <div class="actions flex gap">
+        <button class="btn icon medium primary">
+          <i class="far fa-heart"></i>
+        </button>
+        <button class="btn icon medium primary">
+          <i class="far fa-comment"></i>
+        </button>
+
+        <button class="btn icon medium primary">
+          <i class="fas fa-share-alt"></i>
+        </button>
+      </div>
+      <p>sdfbsdfbsfbsfbsdfbsdfbsfb</p>
+      <div class="actions">{/* <input /> */}</div>
+    </div>
+  );
+};
+export { SocialMediaCard };
